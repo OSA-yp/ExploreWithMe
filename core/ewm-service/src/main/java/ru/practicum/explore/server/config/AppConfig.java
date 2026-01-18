a6360cf7fb6353ba6b4368c6882acfbca82aa987
Sergey Okun <okunSA@yandex.ru>
2026-01-18 13:40:37 +0300

    move local configs to server

--- core/ewm-service/src/main/java/ru/practicum/explore/server/config/AppConfig.java
+++ core/ewm-service/src/main/java/ru/practicum/explore/server/config/AppConfig.java
@@ -2,11 +2,9 @@ package ru.practicum.explore.server.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
-@ConfigurationProperties("ewm")
 @Component
 public class AppConfig {
 
